925a2b62070b3b476e944cceb49904f77ac9b1e9
dartbill <billieldartnell@hotmail.com>
2022-06-17 16:02:52 +0100

    add some colours

--- src/components/ContactMeBtn/index.js
+++ src/components/ContactMeBtn/index.js
@@ -11,7 +11,7 @@ export const ContactMeBtn = () => {
     return (
 <>  
         <div className={styles.container} >
-          <h2  >Get in touch!</h2>
+          <h2 className={styles.text} >Get in touch!</h2>
   
           <a href="./contact.html"  role="button" aria-pressed="true">Contact me!</a>
     
