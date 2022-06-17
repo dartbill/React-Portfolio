cb252ffb8da294532880e30b9b777d11309f5429
dartbill <billieldartnell@hotmail.com>
2022-06-17 16:52:55 +0100

    badly add scroll sections

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,17 +7,34 @@ export const HomePage = () => {
 
   return (
     <main>
+      <div className={styles.snapContainer}>
       <title>Billie Dartnell</title> 
+      
+      <section className={styles.scrollSection}>
       <Components.Title/>
+      </section>
+      
+      <section className={styles.scrollSection}>
       <h2 className={styles.text} >A Few Projects</h2>
       <div className={styles.cardContainer}>
       <Components.PortfolioCard/>
       <Components.PortfolioCard/>
       <Components.PortfolioCard/>
       </div>
+      </section>
+
+      <section className={styles.scrollSection}>
       <Components.MoreProjectsBtn/>
+      </section>
+
+      <section className={styles.scrollSection}>
       <Components.ShortAboutMe/>
+      </section>
+
+      <section className={styles.scrollSection} >
       <Components.ContactMeBtn/>
+      </section>
+      </div>
     </main>
   );
 }
