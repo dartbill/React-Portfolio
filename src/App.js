f00d0c26c3a0a29635505155fc224f8616ab4904
dartbill <billieldartnell@hotmail.com>
2022-06-21 16:52:47 +0100

    underconstruction

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ import "./app.css";
 function App() {
   return (
     <>
-      <Routes>
+      <h1 >WEBSITE UNDER CONSTRUCTION</h1>
+      {/* <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Pages.HomePage />} />
           <Route path="about" element={<Pages.AboutPage />} />
           <Route path="contact" element={<Pages.ContactPage />} />
           <Route path="portfolio" element={<Pages.PortfolioPage />}></Route>
         </Route>
-      </Routes>
+      </Routes> */}
     </>
   );
 }
