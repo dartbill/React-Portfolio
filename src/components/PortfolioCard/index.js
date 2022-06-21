ca1de3b51e9fe14b711c76043f2f824cf9b42f24
dartbill <billieldartnell@hotmail.com>
2022-06-21 12:35:24 +0100

    add blurring img

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -7,13 +7,15 @@ export const PortfolioCard = ({ img, website, link, title, info, programs }) => 
 
     return (
         <>
-            <div >
-                <img className={styles.imgs} src={img} alt="" />
-                <div className={` ${styles.container} ${styles.text} ${styles.card} `}>
-                    <h5> <a href={website} >{title}</a></h5>
-                    <p> {info}</p>
-                    <p> {programs}</p>
-                    <a className={styles.link} href={link} >A link to the code</a>
+            <div id="div" className={styles.something}>
+                <img className={` ${styles.images} ${styles.imgs}`} src={img} alt="" />
+                <div>
+                    <div className={` ${styles.container} ${styles.text} `}>
+                        <h5 className={styles.noBG} > <a className={styles.noBG} href={website} >{title}</a></h5>
+                        <p className={styles.noBG}> {info}</p>
+                        <p className={styles.noBG}> {programs}</p>
+                        <a className={styles.link} href={link} >A link to the code</a>
+                    </div>
                 </div>
             </div>
         </>
