a2b39f84278a170dfc00cad25fb5e8158bee5d6c
dartbill <billieldartnell@hotmail.com>
2022-06-23 08:20:35 +0100

    working home button

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,11 +1,12 @@
 import { NavBar } from "../../components";
 
 
-export const Header = ({ about, contact, portfolio }) => {
+export const Header = ({ home, about, contact, portfolio }) => {
 
   return (
     <>
       <NavBar
+        home={home}
         about={about}
         contact={contact}
         portfolio={portfolio} />
--- src/layout/index.js
+++ src/layout/index.js
@@ -3,9 +3,10 @@ import { Header } from './Header';
 // import { Footer } from './Footer';
 import { Outlet } from 'react-router-dom';
 
-export const Layout = ({ about, contact, portfolio }) => (
+export const Layout = ({ home, about, contact, portfolio }) => (
     <>
         <Header
+            home={home}
             about={about}
             contact={contact}
             portfolio={portfolio} />
