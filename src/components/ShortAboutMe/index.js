13b6580695cf7e8e0895e74d7299c7c9b2dfd50b
dartbill <billieldartnell@hotmail.com>
2022-06-25 14:21:54 +0100

    minor changes

--- src/components/ShortAboutMe/index.js
+++ src/components/ShortAboutMe/index.js
@@ -19,7 +19,9 @@ export const ShortAboutMe = () => {
 
                         <div>
                             <div>
-                                <p className={styles.para} >Outside of work I am an avid gym-goer and have completed two tough mudders, I love to read and anything to do with photography and drawing as well as just relaxing, watching my favourite series’ with my cats. I love to travel, my favourite place I have been so far has been Venice with Greece being a close second if only because of the weather!</p>
+                                <p className={styles.para} >I am a graduate from Bournemouth university with a BA in Computer Animation and Technical Arts.<br></br>
+                                    Outside of work I am an avid gym-goer and have completed two tough mudders, I love to read and anything to do with photography and drawing as well as just relaxing, watching my favourite series’ with my cats. <br></br>
+                                    I love to travel, my favourite place I have been so far has been Venice with Greece being a close second if only because of the weather!</p>
                                 <p className={styles.para2}>Skills:
                                     Command Line Interface | Git/GitHub | HTML/CSS | JavaScript | C/C++ | Bootstrap | Houdini | Maya </p>
                                 {/* <a  href="./about.html">A little bit more?</a> */}
