bc40bdacb4e6aba0fb819c60db794a4ded631a93
dartbill <billieldartnell@hotmail.com>
2022-06-26 08:36:48 +0100

    add linkedin and github

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,10 +1,20 @@
 import { NavBar } from "../../components";
 
+import styles from "./style.module.css"
 
 export const Header = ({ home, about, contact, portfolio }) => {
 
   return (
     <>
+      <div className={styles.icons}>
+        <a class="navbar-brand" target="_blank" href="https://www.linkedin.com/in/billie-dartnell-4608bb156/">
+          <i class="devicon-linkedin-plain"></i>
+        </a>
+
+        <a class="navbar-brand" target="_blank" href="https://github.com/dartbill">
+          <i class="devicon-github-original"></i>
+        </a>
+      </div>
       <NavBar
         home={home}
         about={about}
