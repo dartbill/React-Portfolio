3a5e105d0f333f8374ae6cf074bc79348f787dfc
dartbill <billieldartnell@hotmail.com>
2022-06-26 08:12:10 +0100

    add skill icons

--- src/components/ShortAboutMe/index.js
+++ src/components/ShortAboutMe/index.js
@@ -22,8 +22,30 @@ export const ShortAboutMe = () => {
                                 <p className={styles.para} >I am a graduate from Bournemouth university with a BA in Computer Animation and Technical Arts.<br></br>
                                     Outside of work I am an avid gym-goer and have completed two tough mudders, I love to read and anything to do with photography and drawing as well as just relaxing, watching my favourite series’ with my cats. <br></br>
                                     I love to travel, my favourite place I have been so far has been Venice with Greece being a close second if only because of the weather!</p>
-                                <p className={styles.para2}>Skills:
-                                    Command Line Interface | Git/GitHub | HTML/CSS | JavaScript | C/C++ | Bootstrap | Houdini | Maya </p>
+                                {/* <p className={styles.para2}>Skills:</p> */}
+                                <div className={styles.iconContainer}>
+                                    <i class="devicon-javascript-plain"></i>
+                                    <i class="devicon-html5-plain"></i>
+                                    <i class="devicon-css3-plain"></i>
+                                    <i class="devicon-bootstrap-plain"></i>
+                                    <i class="devicon-c-line"></i>
+                                    <i class="devicon-cplusplus-line"></i>
+                                    <i class="devicon-python-plain-wordmark"></i>
+                                    <i class="devicon-react-original-wordmark"></i>
+                                    <i class="devicon-redux-original"></i>
+                                    <i class="devicon-nodejs-plain-wordmark"></i>
+                                    <i class="devicon-npm-original-wordmark"></i>
+                                    <i class="devicon-express-original-wordmark"></i>
+                                    <i class="devicon-jest-plain"></i>
+                                    <i class="devicon-git-plain"></i>
+                                    <i class="devicon-github-original"></i>
+                                    <i class="devicon-heroku-plain-wordmark"></i>
+                                    <i class="devicon-mongodb-plain-wordmark"></i>
+                                    <i class="devicon-postgresql-plain-wordmark"></i>
+                                    <i class="devicon-docker-plain"></i>
+                                    <i class="devicon-vscode-plain"></i>
+                                    <i class="devicon-maya-plain-wordmark"></i>
+                                </div>
                                 {/* <a  href="./about.html">A little bit more?</a> */}
                             </div>
                         </div>
