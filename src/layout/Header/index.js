3a788653c0ba9ee96a2448679086900d0277dfc0
dartbill <billieldartnell@hotmail.com>
2022-06-26 08:55:00 +0100

    fixed icons

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -7,12 +7,12 @@ export const Header = ({ home, about, contact, portfolio }) => {
   return (
     <>
       <div className={styles.icons}>
-        <a class="navbar-brand" target="_blank" href="https://www.linkedin.com/in/billie-dartnell-4608bb156/">
-          <i class="devicon-linkedin-plain"></i>
+        <a className={styles.links} target="_blank" href="https://www.linkedin.com/in/billie-dartnell-4608bb156/">
+          <i className={styles.imgs} class="devicon-linkedin-plain"></i>
         </a>
 
-        <a class="navbar-brand" target="_blank" href="https://github.com/dartbill">
-          <i class="devicon-github-original"></i>
+        <a className={styles.links} target="_blank" href="https://github.com/dartbill">
+          <i className={styles.imgs} class="devicon-github-original"></i>
         </a>
       </div>
       <NavBar
